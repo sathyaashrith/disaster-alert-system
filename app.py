e4ec01589bed207ec0d2048261c9ce31a30daa26
sathyaashrith <ssareddy2005@gmail.com>
2026-03-27 16:21:14 +0530

    Initial Flask setup

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+alerts = []
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/report', methods=['POST'])
+def report():
+    data = request.json
+    alerts.append(data)
+    return jsonify({"message": "Alert added"}), 201
+
+@app.route('/alerts', methods=['GET'])
+def get_alerts():
+    return jsonify(alerts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
